cecb9d6b7fb9946d43e21d422311e0ee219d27ea
madlalakhaya25 <144236210+madlalakhaya25@users.noreply.github.com>
2024-04-04 11:37:24 +0200

    updated files

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\monam\Desktop\meeting app\myenv
+set VIRTUAL_ENV=C:\Users\KM\TranscriptForge\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
